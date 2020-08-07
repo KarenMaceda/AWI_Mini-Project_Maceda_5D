46a85323527c21611e6ebd6631f2e39e86f1431c
KarenMaceda <karen.maceda1@gmail.com>
2020-08-07 12:53:58 -0500

    Mejor navegacion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,18 @@
     <div class="container">
         <div class="row">
             <div-col class="col-md-6">
-                <h1>Karen Maceda gives you the Welcome!</h1>
-                
+                <h1>Karen Maceda welcomes you!</h1>                
             </div-col>
         </div>
+            </br></br>
+        <div class="content">
+        <h2>Admin Resources </h2>
+            <div class="links">
+                <a href="/admin/albums" class="href">Albums</a> </br>
+                <a href="/admin/apps" class="href">Apps</a></br>
+                <a href="/admin/netflixs" class="href">Netflix</a></br>
+                <a href="/admin/toys" class="href">Toys</a></br>
+            </div>
+        </div>        
     </div>
 @endsection
